b8779b3268b79603a989a5df654e0e62dc687f43
temidayo <temmy.tech01@outlook.com>
2024-01-27 02:10:19 +0100

    upgraded angular version and added cors

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+   BrowserModule,
+   HttpClientModule,
+   FormsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
